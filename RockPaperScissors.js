79deb849a6d3449a5ff2bf1ff02815a3a9132a47
Ejcodes <ihmdemon.exe@gmail.com>
2023-10-12 23:18:38 -0400

    Added CSS to the html

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,4 +1,10 @@
+const startButton = document.getElementById("SB")
+const SBL = () => {
+    console.log("You are invoking the start button logic function")
+}
+
 let game = () => {
     console.log("this the game of Rock, Paper, Scissors!");
 }
+
 
