bf4b1152cb35589dda55aafff4095d7d93c360de
Ejcodes <ihmdemon.exe@gmail.com>
2023-10-17 16:25:03 -0400

    coding the logic

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,10 +1,23 @@
-const startButton = document.getElementById("SB")
-const SBL = () => {
-    console.log("You are invoking the start button logic function")
+const startButton = document.getElementById("SB");
+
+const weaponIndex = ["Rock","Paper","Scissors"];
+
+function SBL(){
+    const aiChoice = Math.floor(Math.random() * 3);
+    console.log(weaponIndex[aiChoice])
+    return weaponIndex[aiChoice];
 }
 
-let game = () => {
-    console.log("this the game of Rock, Paper, Scissors!");
+let repsonse =(Ai)=> {
+    if(Ai === 1){
+        console.log("Rock");
+    }else if(Ai === 2){
+        console.log("Paper");
+    }else if (Ai === 3){
+        console.log("Scissors");
+    }else{
+        console.log("error with AI")
+    }
 }
-
+    
 
